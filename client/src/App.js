f201d48213c17c818847e4e05dc9352ff178931c
burningserenity <burningserenity@novo-ordo.com>
2021-06-21 13:44:17 -0400

    Snarkified

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Shitty calculator that does not work
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
